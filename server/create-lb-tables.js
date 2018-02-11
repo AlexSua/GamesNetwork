8e959f6f8c542b83cdece12c5a98105eee1a9c8a
AlexSua <8196195+AlexSua@users.noreply.github.com>
2018-02-11 21:27:24 +0100

    Añadidas relaciones entre objetos

--- server/create-lb-tables.js
+++ server/create-lb-tables.js
@@ -1,8 +1,5 @@
 var server = require('./server');
 var ds = server.dataSources.db;
-var lbTables = ['Usuario', 'AccessToken', 'ACL', 'RoleMapping', 'Role','Juego'];
-ds.automigrate(lbTables, function(er) {
-  if (er) throw er;
-  console.log('Loopback tables [' + lbTables + '] created in ', ds.adapter.name);
-  ds.disconnect();
-});
+var lbTables = ['Usuario', 'AccessToken', 'ACL', 'RoleMapping', 'Role','Juego','JuegosUsuario'];
+// ds.automigrate();
+ds.discoverModelDefinitions();
